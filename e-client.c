d10146c615976bac8900a7f7b718eebbd5d9e390
Matthew Somers <matthew.somers.hi@gmail.com>
2013-05-10 00:23:28 -0700

    excellent progress on both

---
+++ e-client.c
@@ -0,0 +1,140 @@
+#include <stdlib.h> /* exit */
+#include <string.h> /* memset */
+#include <unistd.h> /* close */
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <time.h>
+
+#define TIMESLOTS 5000
+#define TIMEOUT 4; //seconds to wait for ack before moving on
+#define PORT 9930
+#define BUFLEN 1024
+
+/**
+* TODO: expBackOff() algorithm
+*
+*
+**/
+
+double get_time_ms();
+int expBackOff(int lambda, int something);
+
+int main(int argc, char *argv[])
+{
+   //check args
+   if (argc != 3) 
+   {
+      fprintf(stderr,"usage: %s ip lambda\n", argv[0]);
+      exit(0);
+   }
+
+   int lambda = argv[2];
+
+   struct sockaddr_in si_other, si_me;
+   int s, i, slen=sizeof(si_other);
+   char buf[BUFLEN];
+   char buf2[BUFLEN]; //second buffer to receive acks
+     
+   //socket()
+   s = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+     
+   //complicated stuff for sending
+   memset((char *) &si_other, 0, sizeof(si_other));
+   si_other.sin_family = AF_INET;
+   si_other.sin_port = htons(PORT);
+     
+   //complicated stuff for binding and receiving
+   memset( (char *) &si_me, 0, sizeof(si_me) );
+   si_me.sin_family = AF_INET;
+   si_me.sin_port = htons(PORT);
+   si_me.sin_addr.s_addr = htonl(INADDR_ANY);
+     
+   //bind() because it needs to receive
+   bind(s, &si_me, sizeof(si_me));
+     
+   //inet_aton() converts ip entered
+   inet_aton(argv[1], &si_other.sin_addr);
+     
+   //timeout
+   struct timeval tv;
+   tv.tv_sec = TIMEOUT; 
+   setsockopt(s, SOL_SOCKET, SO_RCVTIMEO, (char *)&tv, sizeof(struct timeval));
+
+   int success = 0;
+   int lostpackets = 0;
+   int waitslots = 0; //send packet to start
+   int something = 0; //something for algorithm probably
+
+   //start clock
+   double starttime = get_time_ms();
+
+   //main loop, sends a packet, waits for an ack
+   for (i = 0; i < TIMESLOTS; i++)
+   {
+      if (waittime == 0)
+      {
+         printf("Sending packet %d\n", i);
+         sprintf(buf, "This is packet %d\n", i);
+         
+         sendto(s, buf, BUFLEN, 0, &si_other, slen);
+         recvfrom(s, buf2, BUFLEN, 0, &si_other, &slen);
+
+         if (strcmp("SUCCESS", buf2) == 0) 
+         {
+            success++;
+            printf("%d. Server received packet successfully.\n\n", i);
+         }
+
+         else if (strcmp("COLLISION", buf2) == 0) //it collided with something!
+         {
+            waitslots = expBackOff(lambda, something);
+            printf("%d. Packet collided, backing off for %d slots.\n", i);
+         }
+
+         else //packet lost, shouldn't happen much with small size
+         {
+            lostpackets++;
+            printf("%d. Packet lost.\n\n", i);
+         }
+
+         bufzero(buf2, BUFLEN); //empty receive buffer at end
+      }
+      else
+      {
+         printf("%d. Waiting %d more slot(s).\n", i, waitslots);
+         waitslots--;
+      }
+         
+   }
+    
+   //end clock
+   double endtime = get_time_ms();
+   double resulttime = (endtime - starttime) / success;
+   int throughput = (success / TIMESLOTS);
+
+   printf("Packets sent: %d\n", success);
+   printf("Packets lost: %d\n", lostpackets);
+   printf("Average RTT (including backoff): %f milliseconds\n", resulttime);
+   printf("Throughput: %d\n", throughput);
+
+   close(s);
+}
+
+double get_time_ms()
+{
+   struct timeval t;
+   gettimeofday(&t, NULL);
+   return (t.tv_sec + (t.tv_usec / 1000000.0)) * 1000.0;
+}
+
+int expBackOff(int lambda, int something)
+{
+   //algorithm goes here
+   return 0;
+}
+
+
---
+++ e-server.c
@@ -0,0 +1,98 @@
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+#define PORT 9930
+#define BUFLEN 1024
+#define TIMEOUT 800000 //in usec, aka 8000ms
+
+/**
+* TODO: test it!
+*
+**/
+
+int main(void)
+{
+   struct sockaddr_in si_me, si_other, si_other2;
+   int s, i, slen=sizeof(si_other);
+   char buf[BUFLEN];
+
+   // socket()
+   s = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+
+   //complicated stuff for receiving
+   memset((char *) &si_other, 0, sizeof(si_other));
+   si_other.sin_family = AF_INET;
+   si_other.sin_port = htons(PORT);
+
+   memset((char *) &si_other2, 0, sizeof(si_other2));
+   si_other2.sin_family = AF_INET;
+   si_other2.sin_port = htons(PORT);
+
+   //complicated stuff for sending acks back
+   memset( (char *) &si_me, 0, sizeof(si_me) );
+   si_me.sin_family = AF_INET;
+   si_me.sin_port = htons(PORT);
+   si_me.sin_addr.s_addr = htonl(INADDR_ANY);
+
+   //bind()
+   bind(s, &si_me, sizeof(si_me));
+
+   //timeout socket so it can send
+   tv.tv_sec = TIMEOUT; 
+   setsockopt(s, SOL_SOCKET, SO_RCVTIMEO, (char *)&tv, sizeof(struct timeval));
+
+   int received = 0;
+   string secondaddr;
+
+   //main loop, packets collide if within 800ms of each other
+   while (1) 
+   {
+      recvfrom(s, buf, BUFLEN, 0, &si_other, &slen);
+
+      if (strcmp("", buf) != 0) //we received something
+      {
+         if (received == 0)
+         {
+            si_other2 = si_other; //apparently this works
+            received++; //first received now
+         }
+
+         else //both received, COLLISION!
+         {
+            printf("Collision occured between packets.\n");
+
+            sprintf(buf, "COLLISION");
+            printf("Sending \"COLLISION\" to %s\n", inet_ntoa(si_other.sin_addr));
+            printf("Sending \"COLLISION\" to %s\n", inet_ntoa(si_other2.sin_addr));
+
+            sendto(s, buf, BUFLEN, 0, &si_other, slen);
+            sendto(s, buf, BUFLEN, 0, &si_other2, slen);
+            received = 0;
+         }
+      }
+      
+      else //nothing received in one cycle
+      {
+         if (received == 1) //one packet received earlier, no collision
+         {
+            printf("Successfully received packet from %s:%d\n", 
+               inet_ntoa(si_other2.sin_addr), ntohs(si_other2.sin_port));
+
+            printf("Sending \"SUCCESS\" to %s\n\n", inet_ntoa(si_other2.sin_addr));
+            
+            sprintf(buf, "SUCCESS");
+            sendto(s, buf, BUFLEN, 0, &si_other, slen);
+            received = 0;
+         }
+      }
+
+      bzero(buf, BUFLEN); //empty buffer at end
+   }
+
+   close(s);
+   return 0;
+}
